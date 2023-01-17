e79f9e92e93868aa6453b48ef6e5cff1fa3abda9
hauszmanndavid <93594997+hauszmanndavid@users.noreply.github.com>
2023-01-17 10:16:33 +0100

    asd

--- feladat.py
+++ feladat.py
@@ -1,9 +1,23 @@
-f = open("szam.txt","r")
+f = open("kerdes.txt","r")
 
-Ladatok = f.readlines()
-Ljavitott = []
-for adat in Ladatok:
-    Ljavitott.append(int(adat.rstrip()))
+feladatok = f.readlines()
 
-print(Ljavitott)
-
+for c in feladatok:
+    for betu in c:
+        if(betu!='*'):
+            print(betu,end="")
+            
+print("\n")
+for kerdes in feladatok:
+    helye = kerdes.index('*')
+    print(helye)
+    jo=kerdes[helye+1]
+    
+    
+tipp = input()
+if(tipp==jo):
+    print("Gratula")
+else:
+    print("Rossz")
+print("Jó válasz: ",jo)
+varakozas = input()
