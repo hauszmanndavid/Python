15341a1c93076a102e433907b1d4b3ec11326307
hauszmanndavid <93594997+hauszmanndavid@users.noreply.github.com>
2023-01-31 10:49:36 +0100

    asd

---
+++ 01.31/kosar.py
@@ -0,0 +1,50 @@
+#1 Csapat átlag magassága (2 tizedes)
+#2 keresés mezszém alapján
+#3 hány center, bedobó, irányító van a csapatban?
+
+file = open("jatekos2.txt","r")
+
+Lfile = []
+
+for i in file:
+    if(i[-1]=='\n'):
+        Lfile.append(i[:-1].split('\t'))
+    else:
+        Lfile.append(i.split('\t'))
+  
+del Lfile[0]
+
+#vessző csere pontra:
+for i in range(len(Lfile)):
+    csere = ''
+    for j in range(len(Lfile[i][1])):
+        if(Lfile[i][1][j]==','):
+            csere+='.'
+        else:
+            csere+= Lfile[i][1][j]
+    Lfile[i][1] = float(csere)
+    
+print(Lfile)
+
+#átlag magasság:
+osszeg=0
+for i in range(len(Lfile)):
+   osszeg += int(Lfile[i][2])
+   
+print("A játékosok átlag magassága: ", round(osszeg/len(Lfile),2))
+#keresés mezszám alapján:
+keres = (input("Keresett mezszám: "))
+
+for keres in range(len(Lfile)):
+    if(keres == (Lfile[i][1])):
+        print(Lfile[i][0]+"\t"+str(Lfile[i][1])+Lfile[i][2])
+    else:
+        print("Ilyen mezszám nem található a csapatban")
+
+
+#3
+for i in range(len(Lfile)):
+    poszt.append(Lfile[i][3])
+    
+print("Ennyi Center: ", poszt.count("center"),"Ennyi Bedobó: ", poszt.count("bedobó"),"Ennyi irányitó: ", poszt.count("irányító"))
+    
